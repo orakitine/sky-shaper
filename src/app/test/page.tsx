637f580e6f7c619b7aa07ac8f34d993ba812a88c
Oleg Rakitine <oleg@rakitine.com>
2024-06-28 22:06:05 -0400

    Remove unused code and add skeleton to the nutrition card

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,6 +1,5 @@
 import { NutrientDetailsCard } from "@/components/llm/nutrition-details-card";
 import React from "react";
-import { nSample } from "../../components/n-sample";
 import { FeedBack } from "@/components/feedback";
 import { NutrientDetailsCardSkeleton } from "@/components/llm/nutrition-details-card-skeleton";
 
@@ -8,8 +7,6 @@ export default function Test() {
   return (
     <>
       <NutrientDetailsCardSkeleton />
-      <hr className="my-8" />
-      <NutrientDetailsCard nutrientDetails={nSample} />
       <hr className="my-8" />
       <FeedBack />
     </>
