92d6b95d5b979e3c94f1ba837bfd18cbe3c63dde
Oleg Rakitine <oleg@rakitine.com>
2024-07-10 10:15:38 -0400

    Rename callback route to confirm-email route

---
+++ src/app/(auth)/confirm-email/route.ts
@@ -0,0 +1,31 @@
+import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+  const requestUrl = new URL(request.url);
+  const code = requestUrl.searchParams.get("code");
+  const type = requestUrl.searchParams.get("type");
+
+  console.log("confirm-email code", code);
+
+  if (code && type === "signup") {
+    try {
+      const supabase = createRouteHandlerClient({ cookies });
+      await supabase.auth.exchangeCodeForSession(code);
+      return NextResponse.redirect(
+        new URL("/email-confirmed?status=success", request.url)
+      );
+    } catch (error) {
+      console.error("Error confirming email:", error);
+      return NextResponse.redirect(
+        new URL("/email-confirmed?status=error", request.url)
+      );
+    }
+  }
+
+  // If no code or wrong type, redirect to an error page or home page
+  return NextResponse.redirect(
+    new URL("/email-confirmation-failed", request.url)
+  );
+}
