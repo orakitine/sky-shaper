705ec3fe36ef6a7a209ffd3ea35d1d833c4cdd5e
Oleg Rakitine <oleg.rakitine@rangle.io>
2024-09-09 17:00:48 -0400

    Upgrade airbender version

--- src/lib/airbender/airbender.utils.server.ts
+++ src/lib/airbender/airbender.utils.server.ts
@@ -1,28 +1,30 @@
 "use server";
 import { env } from "@/env";
-
+import { createServerClient } from "@/lib/supabase/server";
 import { headers } from "next/headers";
 import { airbender } from "./airbender.setup";
-
 export const getAirbenderSessionFromServer = async (productKey?: string) => {
-  let forwardedIp = headers().get("x-forwarded-for");
-  let realIp = headers().get("x-real-ip");
+  const supabase = createServerClient();
 
-  if (forwardedIp) {
-    return await airbender.fetchSession(
-      productKey || env.NEXT_PUBLIC_AIRBENDER_PRODUCT_KEY,
-      forwardedIp
-    );
-  } else if (realIp) {
-    return await airbender.fetchSession(
-      productKey || env.NEXT_PUBLIC_AIRBENDER_PRODUCT_KEY,
-      realIp
-    );
+  const headersList = headers();
+  const ipAddress =
+    headersList.get("x-forwarded-for") ||
+    headersList.get("x-real-ip") ||
+    "no ip set";
+  const finalProductKey = productKey || env.NEXT_PUBLIC_AIRBENDER_PRODUCT_KEY;
+  const user = await supabase.auth.getUser();
+  const userData = user.data.user;
+  if (!userData) {
+    throw new Error("User not found");
   }
+  const userName = userData.user_metadata.name;
 
-  // no ip set
-  return await airbender.fetchSession(
-    productKey || env.NEXT_PUBLIC_AIRBENDER_PRODUCT_KEY,
-    "no ip set"
-  );
+  const sessionId = await airbender.fetchSession({
+    productKey: finalProductKey,
+    ipAddress,
+    name: userName,
+  });
+
+  console.log("sessionId", sessionId.id);
+  return sessionId;
 };
